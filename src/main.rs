f453f6341150c62d52d5f2199c208955682beb2a
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-12-24 13:41:04 +0000

    chore: adds command not found

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+
+    println!("{}: command not found", buf.trim_end());
 }
