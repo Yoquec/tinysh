0a81fb92d3a4d50380838949a1a5e9b1b79060bf
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-12-24 14:28:55 +0000

    chore: implements type command

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+const BUILTINS: [&str; 3] = ["echo", "type", "exit"];
+
 fn main() {
-    loop {
+    'mainloop: loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
@@ -10,9 +12,31 @@ fn main() {
         io::stdin().read_line(&mut buf).unwrap();
 
         match &buf.trim().split_whitespace().collect::<Vec<_>>()[..] {
-            ["exit", ..] => break,
-            ["echo", rest @ ..] => println!("{}", rest.join(" ")),
-            _ => println!("{}: command not found", buf.trim_end()),
+            [command, arguments @ ..] => match *command {
+                "echo" => echo(arguments),
+                "type" => type_(arguments),
+                "exit" => break 'mainloop,
+                _ => print_not_found(command),
+            },
+            _ => continue,
         }
     }
+}
+
+fn print_not_found(command: &str) {
+    println!("{}: command not found", command.trim())
+}
+
+fn type_(arguments: &[&str]) {
+    for arg in arguments {
+        if BUILTINS.contains(arg) {
+            println!("{} is a shell builtin", *arg)
+        } else {
+            println!("{}: not found", *arg)
+        }
+    }
+}
+
+fn echo(arguments: &[&str]) {
+    println!("{}", arguments.join(" "))
 }
